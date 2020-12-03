eb50bca2c0fb297139d95cf4c6f69ad70587304d
Justine Licuanan <caissoone@gmail.com>
2020-12-03 21:40:02 +0800

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: `Mobile Legends Index`,
+    description: `an up-to-date Mobile Legends Index with hero list, stats, and more.`,
+    author: `Justine Licuanan`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
